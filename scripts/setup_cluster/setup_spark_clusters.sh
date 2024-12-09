3950e19c2eb80efcbcf305a07f8bd1e0b1d56a5c
Abdelhadi Nasmane <abdelhadi.nasmane@grenoble-inp.org>
2024-12-09 11:14:23 +0100

    First push in save-me

--- scripts/setup_cluster/setup_spark_clusters.sh
+++ scripts/setup_cluster/setup_spark_clusters.sh
@@ -171,6 +171,8 @@ clone_repo() {
     ssh_exec "$SITE" "$NODE" "
         rm -rf ~/systemes-distribues/
         git clone https://github.com/abdonasmane/systemes-distribues.git
+        cd ~/systemes-distribues/
+        git checkout 8e12bb3510b3d7afd0113d2d324974472aad3a74
     "
 }
 
